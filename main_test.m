cf149b48c48515da3cf65d9b3527aaaf3379532a
eliabonte <elia.bontempelli@studenti.unitn.it>
2022-12-28 21:02:26 +0100

    Update main_test.m

--- main_test.m
+++ main_test.m
@@ -18,10 +18,12 @@ ls  = laserscans{1};                                % choose a laserscan
 obj = Laserscan(ls.r, (ls.Theta) .* 3.14 / 180);    % build the object
 
 extracted_features = obj.extract_feature();         % extract the features
-
-%figure(1), clf, hold on;
-%plot(obj)
-
 obj.features = extracted_features;
-figure(2), clf, hold on;
-plot(obj);
+
+figure(1), clf, hold on;
+plot(obj)
+for k = 1:size(extracted_features, 1)
+    p1 = obj.cartesian(extracted_features(k, 1), :);
+    p2 = obj.cartesian(extracted_features(k, 2), :);
+    plot([p1(1), p2(1)], [p1(2), p2(2)], 'o-r', 'LineWidth', 1);
+end
