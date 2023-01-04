ba5ea3128401692162b7afcf46c152867d9d81fe
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-01-04 11:42:51 +0100

    Improved Odometry class Defined properties and constructor Built a function that, given the current robot position, allows to update it based on the odometry collected information

--- Classes/Odometry.m
+++ Classes/Odometry.m
@@ -7,7 +7,10 @@ classdef Odometry < handle
 % /_/   \_\__|\__|_|  |_|_.__/ \__,_|\__\___||___/
 %                                                 
 properties 
-                 
+    dx;         % x increment
+    dy;         % y increment
+    dtheta;     % theta increment
+    dX;         % column vector of the 3 increments
 end % properties
 
 %  ____        _     _ _        __  __                _                                                             
@@ -20,7 +23,31 @@ end % properties
 % here we firstly define the functions that are intended to be called in the main program                                                                                                                   
 methods 
 
-    function obj = Odometry() % constructor
+    % Take as input the 3 odometry data and set the properties of the class
+    function obj = Odometry(data) % constructor
+        obj.dx     = data(1);     
+        obj.dy     = data(2);     
+        obj.dtheta = data(3);
+        obj.dX     = [obj.dx; obj.dy; obj.dtheta];
+    end
+
+
+    % Given the current state X_curr of the robot, it computes the updated state X_next based on the
+    % odometry dX. It's also able to return the jacobian of such transformation.
+    % Based on the update function X_next = f(X_curr, dX) described as
+    %       x_next     = x_curr + cos(theta_curr) * dx - sin(theta_curr) * dy
+    %       y_next     = y_curr + sin(theta_curr) * dx + cos(theta_curr) * dy
+    %       theta_next = theta_curr + dtheta
+    % that can be rewritten in a form X_next = X_curr + A*dX, where A is actually the jacobian of 
+    % the transformation.
+    function [X_next, jac] = update_states(obj, X_curr) 
+
+        theta  = X_curr(3);
+        jac    = [ cos(theta), -sin(theta), 0; ...
+                   sin(theta),  cos(theta), 0; ...
+                   0,           0,          1  ];
+        X_next = X_curr + jac*obj.dX;
+
     end
     
 
