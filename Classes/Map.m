99e3c7dc11f929402ddfbee52d5f633524100a09
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-01-05 14:05:36 +0100

    #7 coded the compute_innovation function Added check that the correspondence vector must be equal Initialized vectors and matrices based on their dimension Added iteration on all proper observation-landmark pairs to build the desired information Ready for pull request

--- Classes/Map.m
+++ Classes/Map.m
@@ -8,7 +8,7 @@ classdef Map < handle
 %                                                 
 properties 
 
-    landmark_vector;
+    landmark_vector;        % a vector of Landmark objects
     laserscan_buffer;
                  
 end % properties
@@ -26,18 +26,51 @@ methods
     function obj = Map() % constructor
     end
 
+    
     % Given the map and a robot placed inside it that provides a vector of observations, this
     % function should be able to generate:
     %  - the innovation vector z;
     %  - the Jacobian of the observation function H_x;
     %  - the covariance matrix R of the measurement.
+    % Reference to equation taken from:
+    % https://www.iri.upc.edu/people/jsola/JoanSola/objectes/curs_SLAM/SLAM2D/SLAM%20course.pdf
     function [z, H_x, R] = compute_innovation(map, robot, observation_vector)
 
         P1, P2 = obj.compute_correspondences(robot, observation_vector)
 
-        %% TODO
+        if(length(P1) ~ length(P2))
+            error([ 'The number of correspondences is not the same for P1 and P2,',
+                    ' could not compute the innovation vector']);
+        end
 
+        % Initialization:
+        dim_z   = 2 * length(P1);       % dimension of the observation vector
+        dim_P   = 3 + 2 * map.size();   % dimension of the P matrix (robot pose + (2x)landmarks)
+
+        z       = zeros(dim_z, 1);
+        H_x     = zeros(dim_z, dim_P);
+        R       = zeros(dim_z, dim_z);
+
+        for k = 1:length(P1)
+
+            i_obs       = P1(k);    % index of the measurement inside the observation vector
+            i_land      = P2(k);    % index of the landmark inside the map
+            observation = observation_vector(i_obs);    % current observation of interest
+            landmark    = map.landmark_vector(i_land);  % current landmark of interest
+
+            % Compute the estimated observation (with jacobian) bases on the current robot pose 
+            % estimation and the landmark position estimation
+            h, Jh_x_rob, Jh_x_land = robot.landmark_observation(landmark);
+
+            % Fill the innovation vector, the Jacobian and the covariance matrix
+            z(2*k-1:2*k)                            = observation.z - h;        % eq (19, 26)
+            H_x(2*k-1:2*k, 1:3)                     = Jh_x_rob;                 % eq (25)
+            H_x(2*k-1:2*k, 3+2*i_land-1:3+2*i_land) = Jh_x_land;                % eq (25)
+            R(2*k-1:2*k, 2*k-1:2*k)                 = observation.R;
+
+        end
     end
+
 
     % The objective of this function is to compute the correspondence between the landmarks 
     % (contained in the map object itself) and the observations coming from a robot.
