bb40885df376fbc0e16c1606b2b0be71501b58a7
edoardo <edoardo.castioni@gmail.com>
2023-01-22 18:51:22 +0100

    added plot of SVD

--- main_test.m
+++ main_test.m
@@ -18,7 +18,7 @@ load_data
 
 %% SVD estimation
 clc;
-index = 4000;
+index = 14000;
 delta_index = 5;
 
 ls1  = laserscans{index};
@@ -33,3 +33,21 @@ end
 alpha = svd_estimation(ls1, ls2);
 fprintf("SVD estimation = %f\n", alpha);
 fprintf("sum of odometries = %f\n\n", dtheta_tot);
+
+dtheta_tot = zeros(length(laserscans)-delta_index,1);
+
+for i = 1:length(laserscans) - delta_index
+    ls1  = laserscans{i};
+    ls2  = laserscans{i+delta_index};
+    index = i;
+    for j = index:index+delta_index
+        dtheta_tot(i) = dtheta_tot(i) + odometries{j}.dtheta; 
+    end
+
+    alpha(i) = svd_estimation(ls1, ls2);
+
+end
+
+plot(1:1:length(alpha),alpha);
+hold on
+plot(1:1:length(alpha),dtheta_tot);
