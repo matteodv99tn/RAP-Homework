e1cca576e8d9f849d6baf4efe6dd398e6931eaca
edoardo <edoardo.castioni@gmail.com>
2023-01-24 10:21:24 +0100

    final commit

--- main_test.m
+++ main_test.m
@@ -1,6 +1,5 @@
 %% Initialization
-close all;
-clear;  
+close all;  
 clc;
 
 addpath('Data')
@@ -12,14 +11,14 @@ addpath('Classes')
 config
 
 % Load the data
-load_precomputed_data = true;
-load_data
+% load_precomputed_data = true;
+% load_data
 
 
 %% SVD estimation
 clc;
 index = 14000;
-delta_index = 5;
+delta_index = 1;
 
 ls1  = laserscans{index};
 ls2  = laserscans{index+delta_index};
@@ -36,18 +35,29 @@ fprintf("sum of odometries = %f\n\n", dtheta_tot);
 
 dtheta_tot = zeros(length(laserscans)-delta_index,1);
 
-for i = 1:length(laserscans) - delta_index
+alphas = [];
+alphas_idx = [];
+alpha_old = 0;
+for i = 1:delta_index:length(laserscans)-delta_index
     ls1  = laserscans{i};
     ls2  = laserscans{i+delta_index};
-    index = i;
-    for j = index:index+delta_index
-        dtheta_tot(i) = dtheta_tot(i) + odometries{j}.dtheta; 
-    end
 
-    alpha(i) = svd_estimation(ls1, ls2);
-
+    alpha = wrapToPi(svd_estimation(ls1, ls2));
+    alphas = [alphas, alpha_old + alpha];
+    alphas_idx = [alphas_idx, i];
+    alpha_old = alpha_old + alpha;
 end
 
-plot(1:1:length(alpha),alpha);
-hold on
-plot(1:1:length(alpha),dtheta_tot);
+figure(), clf, hold on;
+plot(alphas_idx, alphas);
+plot(th);
+plot(gt(:, 4));
+legend(['SVD estimate'; 'EKF estimate'; 'Ground truth']);
+grid on;
+xlabel('time index');
+ylabel('theta [rad]');
+
+% plot(1:1:length(alpha),alpha);
+
+% hold on
+% plot(1:1:length(alpha),dtheta_tot);
